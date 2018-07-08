81acc4f16f8528b02a4cf1a4d9cbb5d0ce5b9fc2
luckyRoGGeR <warlack@outlook.com>
2018-07-08 19:14:12 +0300

    initial commit

---
+++ src/components/form.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+
+export default class Form extends Component {
+    state = {
+        totalAmount: 1000000
+    };
+    handleChange(event) {
+
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+    render() {
+        return (
+            <form>
+                <div className="attrWrapper">
+                    <label htmlFor="totalAmount">Total amount</label>
+                    <input type="number" name="totalAmount" value={this.state.totalAmount} onChange={this.handleChange.bind(this)}/>
+                </div>
+                <div className="attrWrapper">
+                    <label htmlFor="procents">Procents</label>
+                    <input type="number" name="procents"/>
+                </div>
+            </form>
+        )
+    }
+}

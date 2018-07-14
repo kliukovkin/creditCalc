f10e6ea82f50385bc6dac21de02c13224f6fdd73
luckyRoGGeR <warlack@outlook.com>
2018-07-14 18:04:05 +0300

    add bootstrap

--- src/components/form.js
+++ src/components/form.js
@@ -1,28 +1,45 @@
 import React, {Component} from 'react';
-import './form.css'
+import {Form, FormControl, FormGroup, Col, ControlLabel} from 'react-bootstrap';
 
-export default class Form extends Component {
+export default class myForm extends Component {
     state = {
         totalAmount: 1000000
     };
-    handleChange(event) {
 
+    handleChange(event) {
+        console.log(event.target.name, this.state);
         this.setState({
             [event.target.name]: event.target.value
         });
     }
+
     render() {
         return (
-            <form>
-                <div className="attrWrapper">
-                    <label htmlFor="totalAmount">Total amount</label>
-                    <input type="number" name="totalAmount" value={this.state.totalAmount} onChange={this.handleChange.bind(this)}/>
-                </div>
-                <div className="attrWrapper">
-                    <label htmlFor="procents">Procents</label>
-                    <input type="number" name="procents"/>
-                </div>
-            </form>
+            <Form horizontal width={800}>
+                <FormGroup controlId="formHorizontalEmail">
+                    <Col componentClass={ControlLabel}>
+                        Total amount
+                    </Col>
+                    <Col>
+                        <FormControl
+                            type="number"
+                            name="totalAmount"
+                            placeholder="Total amount"
+                            value={this.state.totalAmount}
+                            onChange={this.handleChange.bind(this)}
+                        />
+                    </Col>
+                </FormGroup>
+
+                <FormGroup controlId="formHorizontalPassword">
+                    <Col componentClass={ControlLabel} >
+                        Procents
+                    </Col>
+                    <Col>
+                        <FormControl type="number" placeholder="Procents"/>
+                    </Col>
+                </FormGroup>
+            </Form>
         )
     }
 }

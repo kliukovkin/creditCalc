ff0ff09d8018aa483da12ea4b4578252a5ed66b4
luckyRoGGeR <warlack@outlook.com>
2018-07-15 22:32:08 +0300

    add eslint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import Form from './components/form/form';
+import { myForm } from './components/form/';
 
 class App extends Component {
   render() {
     return (
-      <Form/>
+      <myForm/>
     );
   }
 }
--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
 import {Form, FormGroup, ControlLabel, Radio} from 'react-bootstrap';
 import NumberFormat from 'react-number-format';
-import './form.css';
+import './style.css';
 
-export default class myForm extends Component {
-    constructor(props){
+export class myForm extends Component {
+    constructor(props) {
         super(props);
         this.state = {
             totalAmount: '',
@@ -23,6 +23,7 @@ export default class myForm extends Component {
             [event.target.name]: value
         });
     }
+
     handleRadioChange(e) {
         this.setState({
             selectedOption: e.target.value
@@ -35,75 +36,75 @@ export default class myForm extends Component {
                 <FormGroup>
                     <ControlLabel>
                         Total amount
-                    </ControlLabel>{" "}
-                        <NumberFormat
-                            displayType={"input"}
-                            thousandSeparator={true}
-                            name="totalAmount"
-                            suffix={" c.u."}
-                            placeholder="Total amount"
-                            value={this.state.totalAmount}
-                            onValueChange={this.handleInput.bind(this)}
-                        />
+                    </ControlLabel>{' '}
+                    <NumberFormat
+                        displayType={'input'}
+                        thousandSeparator={true}
+                        name="totalAmount"
+                        suffix={' c.u.'}
+                        placeholder="Total amount"
+                        value={this.state.totalAmount}
+                        onValueChange={this.handleInput.bind(this)}
+                    />
                 </FormGroup>
                 <FormGroup>
                     <ControlLabel>
                         Percents
-                    </ControlLabel>{" "}
-                        <NumberFormat
-                            displayType={"input"}
-                            decimalScale={1}
-                            fixedDecimalScale={true}
-                            suffix={' %'}
-                            name="percents"
-                            placeholder="Percents"
-                            value={this.state.percents}
-                            isAllowed={(values) => {
-                                const {value} = values;
-                                return value < 100
-                            }}
-                            onValueChange={this.handleInput.bind(this)}
-                        />
+                    </ControlLabel>{' '}
+                    <NumberFormat
+                        displayType={'input'}
+                        decimalScale={1}
+                        fixedDecimalScale={true}
+                        suffix={' %'}
+                        name="percents"
+                        placeholder="Percents"
+                        value={this.state.percents}
+                        isAllowed={(values) => {
+                            const {value} = values;
+                            return value < 100;
+                        }}
+                        onValueChange={this.handleInput.bind(this)}
+                    />
                 </FormGroup>
                 <FormGroup>
                     <Radio
                         name="radioGroup"
                         inline
-                        value={"date"}
-                        checked={this.state.selectedOption === "date"}
+                        value={'date'}
+                        checked={this.state.selectedOption === 'date'}
                         onChange={this.handleRadioChange}>
                         Date
                     </Radio>{' '}
                     <NumberFormat
-                        displayType={"input"}
+                        displayType={'input'}
                         suffix={' month'}
                         name="date"
                         placeholder="Date"
                         value={this.state.date}
                         onValueChange={this.handleInput.bind(this)}
-                        disabled={this.state.selectedOption !== "date"}
+                        disabled={this.state.selectedOption !== 'date'}
                     />
                 </FormGroup>
                 <FormGroup>
                     <Radio
                         name="radioGroup"
                         inline
-                        value={"payment"}
-                        checked={this.state.selectedOption === "payment"}
+                        value={'payment'}
+                        checked={this.state.selectedOption === 'payment'}
                         onChange={this.handleRadioChange}>
-                    Payment
+                        Payment
                     </Radio>{' '}
                     <NumberFormat
-                        displayType={"input"}
-                        suffix={" c.u."}
+                        displayType={'input'}
+                        suffix={' c.u.'}
                         name="payment"
                         placeholder="Payment"
                         value={this.state.payment}
                         onValueChange={this.handleInput.bind(this)}
-                        disabled={this.state.selectedOption !== "payment"}
+                        disabled={this.state.selectedOption !== 'payment'}
                     />
                 </FormGroup>
             </Form>
-        )
+        );
     }
 }
